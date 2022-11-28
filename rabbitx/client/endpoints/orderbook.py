ac48d39ff892361d73fa67000ed2369c4c989bb6
rabbitx-io <114927084+rabbitx-io@users.noreply.github.com>
2022-11-28 14:25:50 +0800

    add trades and orderbook endpoint

--- rabbitx/client/endpoints/orderbook.py
+++ rabbitx/client/endpoints/orderbook.py
@@ -3,12 +3,12 @@ from rabbitx.client.endpoint_group import EndpointGroup
 
 class OrderBookGroup(EndpointGroup):
 
-    def get(self, market_id: str, depth: int = 10):
-        data = dict(method='GET', path='/orderbook')
-        params = dict(marketID=market_id, depth=depth)
+    def get(self, market_id: str):
+        data = dict(method='GET', path='markets/orderbook')
+        params = dict(market_id=market_id)
 
         resp = self.session.session.get(
-            f'{self.session.api_url}/orderbook',
+            f'{self.session.api_url}/markets/orderbook',
             params=params,
             headers=self.session.headers,
         ).json()
--- rabbitx/client/endpoints/trade.py
+++ rabbitx/client/endpoints/trade.py
@@ -8,20 +8,14 @@ class TradeGroup(EndpointGroup):
     def list(
         self,
         market_id: str,
-        timestamp_from: int = None,
-        timestamp_to: int = None,
+        limit:int = 100
     ):
-        params = dict(marketID=market_id)
+        params = dict(market_id=market_id)
 
-        if not all([timestamp_from, timestamp_to]):
-            timestamp_from = int(datetime.utcnow().timestamp()) - 3600 * 24
-            timestamp_to = int(datetime.utcnow().timestamp())
-
-        params['timestampFrom'] = timestamp_from
-        params['timestampTo'] = timestamp_to
+        params['limit'] = limit
 
         resp = self.session.session.get(
-            f'{self.session.api_url}/trades',
+            f'{self.session.api_url}/markets/trades',
             params=params,
             headers=self.session.headers,
         ).json()
