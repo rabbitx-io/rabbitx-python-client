01067d5ea9c602a2be00049c6efa3452f958a75f
rabbitx <info@rabbtix.io>
2023-02-16 21:54:15 +0800

    add dev and prod endpoints

--- rabbitx/const.py
+++ rabbitx/const.py
@@ -1,5 +1,9 @@
-DEV_URL = 'https://api.testnet.rabbitx.io'
-WS_DEV_URL = 'wss://api.testnet.rabbitx.io/ws'
+DEV_URL = 'https://api.dev.rabbitx.io'
+WS_DEV_URL = 'wss://api.dev.rabbitx.io/ws'
+TESTNET_URL = 'https://api.testnet.rabbitx.io'
+WS_TESTNET_URL = 'wss://api.testnet.rabbitx.io/ws'
+URL = 'https://api.rabbitx.io'
+WS_URL = 'wss://api.rabbitx.io/ws'
 
 LOCAL_URL = 'http://localhost:8888'
 
