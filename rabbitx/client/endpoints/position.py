faf402d5eccb1dc215574cac49a328fffc09a96e
rabbitx-io <114927084+rabbitx-io@users.noreply.github.com>
2023-02-17 15:44:29 +0800

    add positions list endpoint

--- rabbitx/client/endpoints/position.py
+++ rabbitx/client/endpoints/position.py
@@ -3,15 +3,12 @@ from rabbitx.client.endpoint_group import EndpointGroup
 
 class PositionGroup(EndpointGroup):
 
-    def list(self, market_id: list[str] = None):
-        market_id = ','.join(market_id or [])
+    def list(self):
         data = dict(method='GET', path='/positions')
         self.session.sign_request(data)
-        params = dict(marketID=market_id,)
 
         resp = self.session.session.get(
             f'{self.session.api_url}/positions',
-            params=params,
             headers=self.session.headers,
         ).json()
 
