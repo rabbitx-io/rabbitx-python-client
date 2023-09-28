a624177e1afc20db387bd2c6af03605e53ca5833
rabbitx-io <114927084+rabbitx-io@users.noreply.github.com>
2023-09-28 16:48:27 +0800

    fix minor bug (#9)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='rabbitx',
-    version='0.1.0',
+    version='0.1.3',
     description='Rabbit API client',
     packages=find_packages(exclude=['tests']),
     install_requires=['requests', 'web3', 'websocket-client', 'rel'],
