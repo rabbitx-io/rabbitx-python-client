78b2c9e3e4186615bd9d7e2958451815b093aeb9
ming <info@rabbitx.io>
2023-12-18 05:11:19 +0000

    change signing message

--- rabbitx/const.py
+++ rabbitx/const.py
@@ -7,5 +7,5 @@ WS_URL = 'wss://api.prod.rabbitx.io/ws'
 
 LOCAL_URL = 'http://localhost:8888'
 
-ONBOARDING_MESSAGE = 'Welcome to Rabbit DEX'
+ONBOARDING_MESSAGE = 'Welcome to RabbitX!\n\nClick to sign in and on-board your wallet for trading perpetuals.\n\nThis request will not trigger a blockchain transaction or cost any gas fees. This signature only proves you are the true owner of this wallet.\n\nBy signing this message you agree to the terms and conditions of the exchange.'
 SIGNATURE_LIFETIME = 300
