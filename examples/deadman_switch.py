17ff02321d191c173655374a875ada315a63a684
ming <info@rabbitx.io>
2024-08-15 08:07:04 +0000

    clean up

--- examples/deadman_switch.py
+++ examples/deadman_switch.py
@@ -4,6 +4,7 @@ from rabbitx.client import Client
 import os
 from dotenv import load_dotenv
 import argparse
+import time
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
@@ -39,21 +40,29 @@ if __name__ == '__main__':
         else:
             client = Client(api_url=const.BFX_URL, api_key=api_key, api_secret=api_secret, public_jwt=public_jwt, private_jwt=private_jwt, exchange=exchange)
 
+    def print_account_equity(client):
+        account_info = client.account.get()
+        print(f'\033[92m\nClient Account Equity:\n\033[0m', account_info['account_equity'])
+
+    def test_deadman_switch(client):
+        result = client.deadman.get()
+        print(f'\033[92m\nDeadman switch status:\n\033[0m', result)
+
+        result = client.deadman.remove()
+        print(f'\033[92m\nDelete deadman return response:\n\033[0m', result)
+
+        result = client.deadman.get()
+        print(f'\033[92m\nDeadman switch status after delete:\n\033[0m', result)
+
+        timeout_ms = 20000  # Set the timeout in milliseconds
+        result = client.deadman.create(timeout_ms)
+        print(f'\033[92m\n\n\nCancel all after {timeout_ms} ms result:\n\033[0m', result)
+
     # Onboarding is needed for private endpoints
     client.onboarding.init()
-    # Print client account information
-    account_info = client.account.get()
-    print(f'\033[92m\nClient Account Equity:\n\033[0m', account_info['account_equity'])
-    # Print client open orders count
-    open_orders = client.orders.list(status=OrderStatus.OPEN)
-    print(f'\033[92m\nClient Open Orders Count:\n\033[0m', len(open_orders))
-    # Test the cancel_all_after endpoint
-    result = client.orders.get_cancel_all_after()
-    print(f'\033[92m\nDeadman switch status:\n\033[0m', result)
-    result = client.orders.delete_cancel_all_after()
-    print(f'\033[92m\nDelete cancel all after result:\n\033[0m', result)
-    timeout_ms = 5000  # Set the timeout in milliseconds
-    result = client.orders.cancel_all_after(timeout_ms)
-    print(f'\033[92m\n\n\nCancel all after {timeout_ms} ms result:\n\033[0m', result)
-    # Test the get_deadman_switch endpoint
     
+    # Print client account information
+    print_account_equity(client)
+    
+    # Test the cancel_all_after endpoint
+    test_deadman_switch(client)
